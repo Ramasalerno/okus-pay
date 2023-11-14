aa6147f4667eb26f817c8df0e90bf22f044efd3c
RamiroSB <98915532+RamiroSB@users.noreply.github.com>
2023-11-14 16:51:27 -0300

    Add files via upload

---
+++ src/components/Footer/FooterMapa.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './FooterMapaStyle.css'
+
+export const FooterMapa = () => {
+  return (
+    <div>
+      
+
+    <div className='footer_1'>
+
+
+    <h2 className='mt-3 mb-3'>ENCONTRANOS!</h2>
+
+    <div className='container'>
+    <iframe className='mapaDvg' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13150.884916326577!2d-58.520577!3d-34.509951!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb0ef83b0a447%3A0x9658c8f3d01056f4!2sAdolfo%20Bellocq%204184%2C%20Olivos%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1666206333541!5m2!1ses-419!2sar" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='mapaDVG'/>
+    </div>
+
+        
+    </div>
+
+    </div>
+  )
+}
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FlexMarcas } from '../FlexMarcas/FlexMarcas';
+import { Carrousel } from '../Carrousel/Carrousel';
+
+import { BannerBodySoporte } from '../BannerBodySoporte/BannerBodySoporte';
+import FlexFeatures from '../FlexFeatures/FlexFeatures';
+import { InfoHome } from '../InfoHome/InfoHome';
+import { BannerBody } from '../BannerBody/BannerBody';
+import { ServiciosCard } from '../ServiciosCard/ServiciosCard';
+import { Bienvenida } from '../Bienvenida/Bienvenida';
+import { PreguntasFrecuentes } from '../PreguntasFrecuentes/PreguntasFrecuentes';
+
+export const HomePage = () => {
+  return (
+    <>
+        <Carrousel/>
+        <Bienvenida/>
+        <FlexFeatures/>
+        <InfoHome/>
+        {/* <BannerBody/> */}
+        {/* <ServiciosCard/> */}
+        <BannerBodySoporte/>
+        <PreguntasFrecuentes/>
+        {/* <FlexMarcas/> */}
+    </>
+  )
+}
