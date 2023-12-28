065e748f9cca2a50c4b957c0e7464be5067bf9de
ramiro <ramiro_salerno@solutionbox.com.ar>
2023-12-28 15:30:54 -0300

    retiro efecto bienvenida

--- src/components/Bienvenida/Bienvenida.js
+++ src/components/Bienvenida/Bienvenida.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import "./BienvenidaStyle.css"
 import { Link } from 'react-router-dom'
-import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 
 export const Bienvenida = () => {
   return (
     <div className='titulosPrincipales mt-5'>
-    <AnimationOnScroll animateIn="animate__bounceInUp">
         <h1 className='titulo'>Explora la innovación de Okus-Pay y contáctanos para más detalles</h1>
             <div>
                 <Link to="/contacto" className='buttonHomeP'>
                     <span>Contactenos!</span>
                 </Link>
             </div>
-    </AnimationOnScroll>
     </div>
 
 )
