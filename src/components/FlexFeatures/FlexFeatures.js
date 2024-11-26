778ad679384e2371f7b01ea6132dc0920ed48a43
ramiro <ramiro_salerno@solutionbox.com.ar>
2024-11-26 12:02:16 -0300

    Elimino texto inicio, cambio boton dorado

--- src/components/FlexFeatures/FlexFeatures.js
+++ src/components/FlexFeatures/FlexFeatures.js
@@ -20,7 +20,7 @@ export default function FlexFeatures() {
                   </span>
                   <h3 className="h3-3col mt-4">Tecnología Biométrica Avanzada</h3>
                   <p className="p-3col">
-                  Descubre la revolucionaria tecnología biométrica de Okus-Pay, una experiencia de pago de último nivel y máxima seguridad. Con la identificación única de la palma de la mano, ofrecemos un método rápido, seguro y estéticamente atractivo, superando las limitaciones de otros métodos de pago.
+                  Descubre la revolucionaria tecnología biométrica de Okus-Pay, una experiencia de pago de último nivel y máxima seguridad. 
                   </p>
                 </div>
                 {/*<!-- center --> */}
@@ -34,7 +34,7 @@ export default function FlexFeatures() {
                   </span>
                   <h3 className="h3-3col mt-4">Seguridad sin Compromisos</h3>
                   <p className="p-3col">
-                  A través de nuestra API de última generación, tus datos se almacenan de forma segura en una base de datos conectada a la nube. Realizamos una validación exhaustiva mediante el sistema SID, que incluye verificación de documento de identidad, vigencia y biometría facial, garantizando una seguridad incomparable.
+                  A través de nuestra API de última generación, tus datos se almacenan de forma segura en una base de datos conectada a la nube. Realizamos una validación exhaustiva mediante el sistema SID.
                   </p>
                 </div>
                 {/*<!-- center --> */}
@@ -48,7 +48,7 @@ export default function FlexFeatures() {
                   </span>
                   <h3 className="h3-3col mt-4">Libertad Total de Pago</h3>
                   <p className="p-3col">
-                  Con OkusPay, los usuarios pueden salir sin preocuparse por el teléfono o la billetera. Olvídate de depender de la batería del teléfono o la conexión a internet. Okus-Pay ofrece libertad total de pago, simplificando la experiencia del usuario.
+                  Con OkusPay, los usuarios pueden salir sin preocuparse por el teléfono o la billetera. Olvídate de depender de la batería del teléfono o la conexión a internet.
                   </p>
                 </div>
                 {/*<!-- center --> */}
@@ -65,7 +65,7 @@ export default function FlexFeatures() {
                   </span>
                   <h3 className="h3-3col mt-4">Soporte Inmediato y Personalizado</h3>
                   <p className="p-3col">
-                  En OkusPay, nos enorgullece ofrecer un soporte excepcional. Contáctanos a través de nuestro soporte web, WhatsApp o línea telefónica. Nuestro equipo está listo para ayudarte, con un tiempo de respuesta promedio inferior a 5 minutos.
+                  En OkusPay, nos enorgullece ofrecer un soporte excepcional. Contáctanos a través de nuestro soporte web, WhatsApp o línea telefónica.
                   </p>
                 </div>
                 {/*<!-- center --> */}
@@ -79,7 +79,7 @@ export default function FlexFeatures() {
                   </span>
                   <h3 className="h3-3col mt-4">Innovando desde el 2023</h3>
                   <p className="p-3col">
-                  OkusPay ha tomado vuelo en el mundo de las fintech desde el 2023, sirviendo a las principales empresas del sector. Aunque aún no contamos con casos de éxito registrados, estamos construyendo el futuro de los pagos biométricos.
+                  OkusPay ha tomado vuelo en el mundo de las fintech desde el 2023, sirviendo a las principales empresas del sector.
                   </p>
                 </div>
                 {/*<!-- center --> */}
@@ -93,7 +93,7 @@ export default function FlexFeatures() {
                   </span>
                   <h3 className="h3-3col mt-4">Libertad en la palma de tu mano</h3>
                   <p className="p-3col">
-                  OkusPay va más allá de los límites convencionales. No necesitas llevar tu teléfono ni billetera. La libertad financiera está literalmente en la palma de tu mano. Olvídate de depender de la batería del teléfono o de la conectividad a internet. Con Okus-Pay, la simplicidad se encuentra en cada transacción.
+                  OkusPay va más allá de los límites convencionales. La libertad financiera está literalmente en la palma de tu mano.
                   </p>
                 </div>
                 </div>
